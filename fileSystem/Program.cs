41131e60a2f29e70062a2c5e3d6a670ef3a59924
H <sikkelh@gmail.com>
2021-10-26 14:05:46 +0300

    File System

---
+++ fileSystem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace fileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootdirectory = @"C:\Users\Monkey\samples";
+            string userDirectory = "wishList";
+
+            string newDirectoryFullPath = @$"{rootdirectory}\{userDirectory}";
+            Console.WriteLine(newDirectoryFullPath);
+
+            Directory.CreateDirectory(newDirectoryFullPath);
+
+        }
+    }
+}
