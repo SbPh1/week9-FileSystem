6e0242491e049be4e8f2b19f7ba840a930577a5e
H <sikkelh@gmail.com>
2021-10-27 09:45:09 +0300

    Create File

--- fileSystem/Program.cs
+++ fileSystem/Program.cs
@@ -8,10 +8,23 @@ namespace fileSystem
         static void Main(string[] args)
         {
             string rootdirectory = @"C:\Users\Monkey\samples";
-            string userDirectory = "wishList";
+            Console.WriteLine("Enter directory name:");
+            string userDirectory = Console.ReadLine();
 
             string newDirectoryFullPath = @$"{rootdirectory}\{userDirectory}";
             Console.WriteLine(newDirectoryFullPath);
+
+            bool directoryExists = Directory.Exists(newDirectoryFullPath);
+
+            if (directoryExists)
+            {
+                Console.WriteLine($"{userDirectory} already exists in {rootdirectory}");
+            }
+            else
+            {
+                Directory.CreateDirectory(newDirectoryFullPath);
+                Console.WriteLine($"Directory {userDirectory} has been created.");
+            }
 
             Directory.CreateDirectory(newDirectoryFullPath);
 
